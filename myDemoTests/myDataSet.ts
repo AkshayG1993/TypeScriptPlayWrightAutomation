b6f1e431ec6a66eef4062fa4be496a98530ca2e5
AkshayG1993 <akshaygirepunje93@gmail.com>
2025-07-14 12:39:14 +0530

    Delete myDemoTests/myDataSet.ts

--- myDemoTests/myDataSet.ts
+++
@@ -1,6 +0,0 @@
-export const loginData = [
-  { username: 'standard_user', password: 'secret_sauce' },
-  { username: 'visual_user', password: 'secret_sauce' },
-  { username: 'error_user', password: 'secret_sauce' }
-
-];
