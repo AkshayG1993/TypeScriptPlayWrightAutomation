a6b01d2d756b3707e8a2640e13eb4e1c57acc581
AkshayG1993 <akshaygirepunje93@gmail.com>
2025-07-14 12:39:02 +0530

    Delete myDemoTests/myInvalidDataSet.ts

--- myDemoTests/myInvalidDataSet.ts
+++
@@ -1,6 +0,0 @@
-export const loginInvalidData = [
-  { username: 'locked_out_user', password: 'secret_sauce',error:'Epic sadface: Sorry, this user has been locked out.' },
-  { username: 'invalid_user', password: 'secret_sauce',error:'Epic sadface: Username and password do not match any user in this service' },
-  { username: '', password: '', error:'Epic sadface: Username is required'},
-  { username: 'error_user', password: '', error:'Epic sadface: Password is required'}
-];
